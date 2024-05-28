27285724e7feb8cfc3868c6abdd8ddf116a07d4b
pavyeu <95545326+pavyeu@users.noreply.github.com>
2024-05-28 11:33:25 +0200

    Add files via upload

---
+++ Pojistenci_projekt/Pojistenci_projekt/Pojistenci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pojistenci_projekt
+{
+    internal class Pojistenci
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public string RodneCislo { get; set; }
+        public string TelefonniCislo { get; set; }
+
+        /// <summary>
+        /// Vytvoření pojištěnce
+        /// </summary>
+        /// <returns>Jméno Přijmení Rodné číslo a telefonní číslo</returns>
+        public override string ToString()
+        {
+            return $"{Jmeno} {Prijmeni}, Rodné číslo: {RodneCislo}, Telefonní číslo: {TelefonniCislo}";
+        }
+
+    }
+}
---
+++ Pojistenci_projekt/Pojistenci_projekt/Program.cs
@@ -0,0 +1,140 @@
+﻿using Pojistenci_projekt;
+using System.Xml;
+
+internal class Program
+{
+    static List<Pojistenci> pojistenci = new List<Pojistenci>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("----------\nPojištěnci\n----------");
+            Console.WriteLine("Prosím vyberte jednu možnost: ");
+            Console.WriteLine("1. Přidat pojištěnce");
+            Console.WriteLine("2. Zobrazit všechny pojištěnce");
+            Console.WriteLine("3. Vyhledat pojištěnce");
+            Console.WriteLine("4. Ukončit program");
+
+            string volba = Console.ReadLine();
+
+           switch (volba)
+            {
+                case "1":
+                    PridatPojistence();
+                        break;
+                case "2":
+                    ZobrazitPojistence();
+                        break;
+                case "3":
+                    VyhedatPojistence();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Neplatná volba, vyberte prosím správnou možnost");
+                break;
+
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// Přidání pojištěnce do listu
+    /// </summary>
+    static void PridatPojistence()
+    {
+        Pojistenci NovyPojistenec = new Pojistenci();
+
+        Console.Write("Zadejte jméno: ");
+        NovyPojistenec.Jmeno = Console.ReadLine();
+        
+        if (!ValidaceJmena(NovyPojistenec.Jmeno))
+        {
+            return;
+        }
+
+        Console.Write("Zadejte příjmení: ");
+        NovyPojistenec.Prijmeni = Console.ReadLine();
+
+        if (!ValidaceJmena(NovyPojistenec.Prijmeni))
+        {
+            return;
+        }
+
+        Console.Write("Zadejte rodné číslo: ");
+        NovyPojistenec.RodneCislo = Console.ReadLine();
+
+        Console.Write("Zadejte telefonní číslo: ");
+        NovyPojistenec.TelefonniCislo = Console.ReadLine();
+
+        pojistenci.Add(NovyPojistenec);
+
+        Console.WriteLine("Pojištěnec byl úspěšně přidán!");
+    }
+
+    /// <summary>
+    /// Výpis pojištěnců
+    /// </summary>
+    static void ZobrazitPojistence()
+    {
+        if(pojistenci.Count == 0)
+        {
+            Console.WriteLine("Není zapsaný žádný pojištěnec!");
+        }
+        else
+        {
+            Console.WriteLine("Seznam pojištěnců: ");
+            foreach(Pojistenci pojistenci in pojistenci)
+            {
+                Console.WriteLine(pojistenci);
+            }
+        }
+    }
+   
+    /// <summary>
+    /// Vyhledání pojištěnce pomocí příjmení
+    /// </summary>
+    static void VyhedatPojistence()
+    {
+        Console.Write("Zadejte prosím příjmení pojištěnce: ");
+        string prijmeni = Console.ReadLine();
+        
+        Pojistenci pojistenec = pojistenci.Find(p => p.Prijmeni == prijmeni);
+
+        if (pojistenec == null)
+        {
+            Console.WriteLine("Pojištěnec s tímto příjmením nebyl nalezen");
+        }
+        else
+        {
+            Console.WriteLine("Nalezený pojištěnec: ");
+            Console.WriteLine(pojistenec);
+        }
+    }
+    /// <summary>
+    /// Funkce na validaci jména
+    /// </summary>
+    /// <param name="jmeno"></param>
+    /// <returns>true/false</returns>
+    static bool ValidaceJmena(string jmeno)
+    {
+        if (string.IsNullOrEmpty(jmeno))
+        {
+            Console.WriteLine("Jméno nesmí být prázdné!");
+            return false;
+        }
+            foreach (char pismeno in jmeno)
+        {
+            if (!char.IsLetter(pismeno))
+            {
+                Console.WriteLine("Jméno nesmí obsahovat číslice!");
+                return false;
+            }
+        }
+
+
+            return true;
+    }
+}
